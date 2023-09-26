fcec8e0eee63d4d609e91f90bd72922fe96695db
Alex Bance <Alex.Bance@justice.gov.uk>
2023-09-26 11:00:26 +0100

    fix: formatting

--- example/data-mgmt-zone-existing-purview.tf
+++ example/data-mgmt-zone-existing-purview.tf
@@ -14,8 +14,8 @@ module "data_mgmt_zone_existing_purview" {
   hub_resource_group_name   = var.hub_resource_group_name
 
   existing_purview_account = {
-    resource_id                    = "/subscriptions/a8140a9e-f1b0-481f-a4de-09e2ee23f7ab/resourceGroups/mi-sbox-rg/providers/Microsoft.Purview/accounts/mi-purview-sbox"
-    managed_storage_account_id     = "/subscriptions/a8140a9e-f1b0-481f-a4de-09e2ee23f7ab/resourceGroups/managed-rg-mi-purview-sbox/providers/Microsoft.Storage/storageAccounts/scanuksouthlyehlok"
+    resource_id                = "/subscriptions/a8140a9e-f1b0-481f-a4de-09e2ee23f7ab/resourceGroups/mi-sbox-rg/providers/Microsoft.Purview/accounts/mi-purview-sbox"
+    managed_storage_account_id = "/subscriptions/a8140a9e-f1b0-481f-a4de-09e2ee23f7ab/resourceGroups/managed-rg-mi-purview-sbox/providers/Microsoft.Storage/storageAccounts/scanuksouthlyehlok"
     identity = {
       principal_id = "487b08af-d9de-4ca3-8cbe-7a232a0e8858"
       tenant_id    = "531ff96d-0ae9-462a-8d2d-bec7c0b42082"
