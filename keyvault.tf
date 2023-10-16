575b8c683f8e7212ef06cadc92bf7817fdc3d65e
Alex Bance <Alex.Bance@justice.gov.uk>
2023-10-16 18:46:34 +0100

    fix: switch kv module reference

--- keyvault.tf
+++ keyvault.tf
@@ -1,5 +1,5 @@
 module "key_vault" {
-  source              = "github.com/hmcts/cnp-module-key-vault?ref=fix%2Fremove-creator-access-policy"
+  source              = "github.com/hmcts/cnp-module-key-vault?ref=master"
   name                = "${local.name}-kv-${var.env}"
   product             = "data-mgmt-zone"
   env                 = var.env
