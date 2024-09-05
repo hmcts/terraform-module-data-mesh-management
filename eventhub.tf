bc01e9fdea9ee485be2ea1a1d0b6b385de5d9e80
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-09-05 15:37:18 +0100

    Update rource group

--- eventhub.tf
+++ eventhub.tf
@@ -1,7 +1,7 @@
 resource "azurerm_eventhub_namespace" "eventhub-namespace" {
   name                = "${var.env}-dlrm-eventhub-ns"
   location            = var.location
-  resource_group_name = azurerm_resource_group.new.name
+  resource_group_name = local.resource_group
   sku                 = var.eventhub_ns_sku
   tags                = var.common_tags
 }
@@ -10,7 +10,7 @@ resource "azurerm_eventhub" "eventhub" {
   for_each            = toset(var.services)
   name                = each.key
   namespace_name      = azurerm_eventhub_namespace.eventhub-namespace.name
-  resource_group_name = azurerm_resource_group.new.name
+  resource_group_name = local.resource_group
   partition_count     = 4
   message_retention   = var.message_retention
 }
@@ -18,7 +18,7 @@ resource "azurerm_eventhub" "eventhub" {
 resource "azurerm_eventhub_namespace_authorization_rule" "eventhub-sender" {
   name                = "dlrm-eventhub-namespace-sender"
   namespace_name      = azurerm_eventhub_namespace.eventhub-namespace.name
-  resource_group_name = azurerm_resource_group.new.name
+  resource_group_name = local.resource_group
   
   listen = false
   send   = true
