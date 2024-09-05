91aba61bb04d8cb427ffd8b55aa8a2f2e13cb6f3
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-09-05 15:29:48 +0100

    Use exisiting rg

--- eventhub.tf
+++ eventhub.tf
@@ -1,13 +1,7 @@
-resource "azurerm_resource_group" "eventhub_resource_group" {
-  name     = "${var.env}-dlrm-eventhub-rg"
-  location = var.location
-  tags     = var.common_tags
-}
-
 resource "azurerm_eventhub_namespace" "eventhub-namespace" {
   name                = "${var.env}-dlrm-eventhub-ns"
   location            = var.location
-  resource_group_name = azurerm_resource_group.eventhub_resource_group.name
+  resource_group_name = azurerm_resource_group.new.name
   sku                 = var.eventhub_ns_sku
   tags                = var.common_tags
 }
@@ -16,7 +10,7 @@ resource "azurerm_eventhub" "eventhub" {
   for_each            = toset(var.services)
   name                = each.key
   namespace_name      = azurerm_eventhub_namespace.eventhub-namespace.name
-  resource_group_name = azurerm_resource_group.eventhub_resource_group.name
+  resource_group_name = azurerm_resource_group.new.name
   partition_count     = 4
   message_retention   = var.message_retention
 }
@@ -24,7 +18,7 @@ resource "azurerm_eventhub" "eventhub" {
 resource "azurerm_eventhub_namespace_authorization_rule" "eventhub-sender" {
   name                = "dlrm-eventhub-namespace-sender"
   namespace_name      = azurerm_eventhub_namespace.eventhub-namespace.name
-  resource_group_name = azurerm_resource_group.eventhub_resource_group.name
+  resource_group_name = azurerm_resource_group.new.name
   
   listen = false
   send   = true
