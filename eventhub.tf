7b6dc8f05c1d477faea7016c92e2d72910b31fb9
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-09-06 14:08:07 +0100

    skip stg env

--- eventhub.tf
+++ eventhub.tf
@@ -1,4 +1,10 @@
+locals {
+  eventhub_ns_sku = var.env == "stg" ? null : var.eventhub_ns_sku
+}
+
 resource "azurerm_eventhub_namespace" "eventhub-namespace" {
+  count = var.env == "stg" ? 0 : 1
+
   name                = "${var.env}-dlrm-eventhub-ns"
   location            = var.location
   resource_group_name = local.resource_group
@@ -7,6 +13,8 @@ resource "azurerm_eventhub_namespace" "eventhub-namespace" {
 }
 
 resource "azurerm_eventhub" "eventhub" {
+  count = var.env == "stg" ? 0 : length(var.services)
+
   for_each            = toset(var.services)
   name                = each.key
   namespace_name      = azurerm_eventhub_namespace.eventhub-namespace.name
@@ -19,7 +27,7 @@ resource "azurerm_eventhub_namespace_authorization_rule" "eventhub-sender" {
   name                = "dlrm-eventhub-namespace-sender"
   namespace_name      = azurerm_eventhub_namespace.eventhub-namespace.name
   resource_group_name = local.resource_group
-  
+
   listen = false
   send   = true
   manage = false
