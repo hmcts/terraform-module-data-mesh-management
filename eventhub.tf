8e2ce94ffc3dbcc6a4e6c1ebb252985eec2600b3
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-09-09 15:51:20 +0100

    Adds in stg

--- eventhub.tf
+++ eventhub.tf
@@ -1,9 +1,4 @@
-locals {
-  services_map = var.env == "stg" ? {} : tomap({ for s in var.services : s => s })
-}
-
 resource "azurerm_eventhub_namespace" "eventhub-namespace" {
-  count               = var.env == "stg" ? 0 : 1
   name                = "${var.env}-dlrm-eventhub-ns"
   location            = var.location
   resource_group_name = local.resource_group
@@ -12,20 +7,18 @@ resource "azurerm_eventhub_namespace" "eventhub-namespace" {
 }
 
 resource "azurerm_eventhub" "eventhub" {
-  for_each            = local.services_map
+  for_each            = toset(var.services)
   name                = each.key
-  namespace_name      = azurerm_eventhub_namespace.eventhub-namespace[0].name
+  namespace_name      = azurerm_eventhub_namespace.eventhub-namespace.name
   resource_group_name = local.resource_group
   partition_count     = 4
   message_retention   = var.message_retention
 }
 
 resource "azurerm_eventhub_namespace_authorization_rule" "eventhub-sender" {
-  count               = var.env == "stg" ? 0 : 1
   name                = "dlrm-eventhub-namespace-sender"
-  namespace_name      = azurerm_eventhub_namespace.eventhub-namespace[0].name
+  namespace_name      = azurerm_eventhub_namespace.eventhub-namespace.name
   resource_group_name = local.resource_group
-
   listen = false
   send   = true
   manage = false
